a11878ac2ed4639e277fe4ea00c5fb9f78239e5f
JaVanGri <jan.van.grimbergergen@uni-duesseldorf.de>
2020-06-08 08:55:38 +0200

    Initial commit

---
+++ src/main/java/webservices/rezeptapp/RezeptappApplication.java
@@ -0,0 +1,13 @@
+package webservices.rezeptapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezeptappApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RezeptappApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/webservices/rezeptapp/RezeptappApplicationTests.java
@@ -0,0 +1,13 @@
+package webservices.rezeptapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezeptappApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
